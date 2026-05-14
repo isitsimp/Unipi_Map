7175b650e95c3016cdbf18e019cd3ca9492d211c
isitsimp <isidoratsimplidaki@gmail.com>
2026-05-14 20:39:20 +0300

    Update unipi_map.js

--- unipi_map.js
+++ unipi_map.js
@@ -52,57 +52,87 @@ let stopMarkers = [];
 
 let layers = {};
 
+
+
+let flag = "Greek"; // Default
+
+document.addEventListener('DOMContentLoaded', () => {
+  const path = window.location.pathname;
+  
+  // If the URL contains 'en', set flag to English
+  if (path.includes('en')) {
+    flag = "English";
+  } else {
+    flag = "Greek";
+  }
+
 /*Buildings*/
-fetch('buildings.json')
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(item => {
-      const icon = icons[item.icon] || icons.default;
+  fetch('buildings.json')
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(item => {
+        const icon = icons[item.icon] || icons.default;
 
-      let popupContent = `<b>${item.title}</b><br>${item.street}<br>`;
-      
-      if (item.image) {
-        popupContent += `<img src='${item.image}' style='width:100%; height:auto;'><br>`;
-      }
+        let building;
+        if (flag === "Greek") {
+          building = `<b>${item.title}</b><br>${item.street}<br>`;
+        } else {
+          //English fields
+          building = `<b>${item.title_en}</b><br>${item.street_en}<br>`;
+        }
+        let popupContent = building;
 
-      if (item.info) {
-        popupContent += `${item.info}<br>`;
-      }
+        if (item.image) {
+          popupContent += `<img src='${item.image}' style='width:100%; height:auto;'><br>`;
+        }
 
-      if (item.amea === true) {
-        popupContent += `<img src='/images/wheelchair.png' alt="Προσβάσιμο για αναπηρικές καρέκλες" style='width:24px; height:24px; vertical-align:right;'><br>`;
-      } else {
-        popupContent += `<img src='/images/no-wheelchair.png' alt="Μη-προσβάσιμο για αναπηρικές καρέκλες" style='width:24px; height:24px; vertical-align:right;'><br>`;
-      }
+        if (item.info) {
+          popupContent += `${item.info}<br>`;
+        }
 
-      popupContent += `<a href="${item.googlemaps}" target="_blank">Προβολή στο Google Maps</a> | <a href="${item.streetview}">StreetView</a>`;
+        if (item.amea === true) {
+          popupContent += `<img src='/images/wheelchair.png' alt="Προσβάσιμο για αναπηρικές καρέκλες" style='width:24px; height:24px; vertical-align:right;'><br>`;
+        } else {
+          popupContent += `<img src='/images/no-wheelchair.png' alt="Μη-προσβάσιμο για αναπηρικές καρέκλες" style='width:24px; height:24px; vertical-align:right;'><br>`;
+        }
 
-      let marker = L.marker([item.lat, item.lng], { icon })
-        .bindPopup(popupContent);
-      
-      
-      buildingMarkers.push(marker);
-      
-      if (item.id === "01") { //FIX
-        marker = marker.openPopup();
+        popupContent += `<a href="${item.googlemaps}" target="_blank">Προβολή στο Google Maps</a> | <a href="${item.streetview}">StreetView</a>`;
+
+        let marker = L.marker([item.lat, item.lng], { icon })
+          .bindPopup(popupContent);
+
+        if (item.id === "01") { //FIX
+          let marker = L.marker([item.lat, item.lng], { icon })
+          .bindPopup(popupContent).openPopup();
           }
-      
-    });
+        buildingMarkers.push(marker);
 
-    layers.buildings = L.layerGroup(buildingMarkers);
-    map.addLayer(layers.buildings);
-    updateControl();
+
+      });
+
+      layers.buildings = L.layerGroup(buildingMarkers);
+      map.addLayer(layers.buildings);
+      updateControl();
+    })
+    .catch(error => console.error('Error loading buildings:', error));
 });
-
+                          
+                          
 /*Stops*/
 fetch('stops.json')
   .then(response => response.json())
   .then(data => {
     data.forEach(item => {
       const icon = icons[item.icon] || icons.default;
+      
+      let popupContent = `<b>${item.title}</b><br>${item.info}<br><a href="${item.googlemaps}" target="_blank">Προβολή στο Google Maps</a> | <a href=${item.streetview}>StreetView</a>`;
 
+      if (item.arrivals) {
+        popupContent += `<br><a href="${item.arrivals}" target="_blank">Δρομολόγια</a>`;
+      }
+      
       const marker = L.marker([item.lat, item.lng], { icon })
-        .bindPopup(`<b>${item.title}</b><br>${item.info}<br><a href="${item.googlemaps}" target="_blank">Προβολή στο Google Maps</a> | <a href=${item.streetview}>StreetView</a>`);
+        .bindPopup(popupContent);
       
       stopMarkers.push(marker);
       
